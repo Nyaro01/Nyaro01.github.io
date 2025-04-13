89fb60f6f915fc7248c02655c202985ec21653d9
Nyaro01 <ben.krueger2003@gmail.com>
2025-04-13 16:59:29 +0200

    Css und Js added

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    const myButton = document.getElementById('b1');
+    myButton.addEventListener('click', () => {
+        document.getElementById('text1').textContent = 'Button clicked! lol';
+    });
